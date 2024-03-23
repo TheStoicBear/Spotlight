7664e813911fe8572583d523927ff1b38429b019
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2024-03-22 18:39:30 -0700

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+-----------------------------------------
+--- Spotlight, Made by FAXES & Slavko ---
+------------ FaxSlav Devs :P ------------
+-----------------------------------------
+
+fx_version 'adamant'
+games { 'gta5' }
+
+version '1.0.0'
+
+author 'FAXES & Slavko | TheStoicBear'
+description 'Stoic-PoliceMenu'
+
+server_script 'server.lua'
+client_script 'client.lua'
